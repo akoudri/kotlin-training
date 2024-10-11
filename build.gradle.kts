10ee8a24f868fd4324d33439f87e1debc5c12e6c
akoudri <ali.koudri@gmail.com>
2024-10-11 09:26:04 +0200

    Adds SQLite dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.21")
+    implementation("org.xerial:sqlite-jdbc:3.46.1.3")
     testImplementation(kotlin("test"))
 }
 
