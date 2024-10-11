1639a62cd64b61bfd37b4e555dc581165b5c6020
akoudri <ali.koudri@gmail.com>
2024-10-11 13:41:51 +0200

    Adds async exercise

---
+++ src/main/kotlin/com/akfc/training/async.kt
@@ -0,0 +1,22 @@
+package com.akfc.training.com.akfc.training
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    println("Requesting operations")
+    val results = (1..5).map { i ->
+        async(Dispatchers.Default) {
+            performOperation(i)
+        }
+    }.awaitAll()
+
+    results.forEachIndexed { index, result ->
+        println("Response $index = $result")
+    }
+}
+
+suspend fun performOperation(id: Int): Int {
+    println("Performing operation $id")
+    delay(3000) // Non-blocking delay
+    return id * 42
+}
