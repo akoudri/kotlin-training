47486cd9a1710369302406f46e719fd346ff2e27
akoudri <ali.koudri@gmail.com>
2024-10-11 10:44:22 +0200

    Adds titanic exercise

---
+++ src/main/kotlin/com/akfc/training/titanic.kt
@@ -0,0 +1,34 @@
+package com.akfc.training.com.akfc.training
+
+import java.util.regex.Pattern
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.*
+
+class Customer
+
+class Titanic {
+    private val customers: MutableList<Customer> = mutableListOf()
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        TODO()
+    }
+
+    fun head(n: Int) {
+        TODO()
+    }
+
+    fun tail(n: Int) {
+        TODO()
+    }
+
+    fun getCustomers(): List<Customer> = customers.toList()
+}
+
+fun main() {
+    TODO("Execute expected requests")
+}
