d4e7efac90d6160f03d5a8dfe27de55efcd46f43
akoudri <ali.koudri@gmail.com>
2025-05-18 14:15:05 +0200

    Improves examples

--- src/main/kotlin/com/akfc/training/race.kt
+++ src/main/kotlin/com/akfc/training/race.kt
@@ -1,16 +1,23 @@
 package com.akfc.training.com.akfc.training
 
 import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.locks.ReentrantLock
 
 fun main() = runBlocking {
     var counter = 0
     val numberOfJobs = 2
-    val numberOfIncrements = 200
+    val numberOfIncrements = 200_000
+    val lock = ReentrantLock()
 
     val jobs = List(numberOfJobs) {
         launch(Dispatchers.Default) {
-            repeat(numberOfIncrements) {
-                counter++
+            if (lock.tryLock()) {
+                repeat(numberOfIncrements) {
+                    counter ++
+                }
+            } else {
+                println("Autre traitement")
             }
         }
     }
